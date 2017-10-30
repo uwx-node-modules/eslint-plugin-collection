5f355ec475c0c222466bfc853d86833392f1c007
uwx <cadrekucra@gmx.com>
2017-10-30 19:03:49 -0200

    use Object.assign instead of destructuring thing

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const modules = [
   require("./eslint-plugin-lmjson"),
 ];
 
-module.exports.processors = modules.map(e => e.processors).reduce((a, b) => { ...a, ...b });
+module.exports.processors = modules.map(e => e.processors).reduce((a, b) => Object.assign({}, a, b ));
