5c67a379ebfecbb9b283534a74c56e700e1fd6c3
uwx <cadrekucra@gmx.com>
2017-12-14 00:16:04 -0200

    move sublime files from json to lmjson

--- eslint-plugin-json/index.js
+++ eslint-plugin-json/index.js
@@ -4,13 +4,13 @@
  * @copyright 2015 Azeem Bande-Ali. All rights reserved.
  * See LICENSE file in root directory for full license.
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var json_parse = require("./json_parse.js");
+var json_parse = require('./json_parse.js');
 
 var lineColumn = require('line-column');
 
@@ -22,42 +22,40 @@ var lineColumn = require('line-column');
 var fileContents = {};
 
 var prepro = {
-    preprocess: function(text, fileName) {
-        fileContents[fileName] = text;
-        return [text];
-    },
-    postprocess: function(messages, fileName) {
-        try {
-            json_parse(fileContents[fileName]);
-            delete fileContents[fileName];
-            return [];
-        } catch (e) {
-            delete fileContents[fileName];
-            var index = lineColumn(e.text, e.at-1);
-            var srcLine = e.text.split('\n')[index.line - 1];
+  preprocess: function(text, fileName) {
+    fileContents[fileName] = text;
+    return [text];
+  },
+  postprocess: function(messages, fileName) {
+    try {
+      json_parse(fileContents[fileName]);
+      delete fileContents[fileName];
+      return [];
+    } catch (e) {
+      delete fileContents[fileName];
+      var index = lineColumn(e.text, e.at - 1);
+      var srcLine = e.text.split('\n')[index.line - 1];
 
-            return [{
-                ruleId: "invalid-json-" + e.message
-                    .toLowerCase()
-                    .replace(/\ba\b/g, '')
-                    .replace(/[^a-z ]/g, '')
-                    .trim()
-                    .replace(/\s+/g, '-'),
-                severity: 2,
-                message: e.message,
-                source: srcLine,
-                line: index.line,
-                column: index.col,
-            }];
-        }
+      return [{
+        ruleId: 'invalid-json-' + e.message
+            .toLowerCase()
+            .replace(/\ba\b/g, '')
+            .replace(/[^a-z ]/g, '')
+            .trim()
+            .replace(/\s+/g, '-'),
+        severity: 2,
+        message: e.message,
+        source: srcLine,
+        line: index.line,
+        column: index.col,
+      }];
     }
+  }
 };
 
 // import processors
 module.exports.processors = {
-    // add your processors here
-    ".json": prepro,
-    ".sublime-settings": prepro,
-    ".sublime-keymap": prepro,
+  // add your processors here
+  '.json': prepro,
 };
 
--- eslint-plugin-lmjson/index.js
+++ eslint-plugin-lmjson/index.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var lmjsonParse = require("lmjson");
+var lmjsonParse = require('lmjson');
 
 //------------------------------------------------------------------------------
 // Plugin Definition
@@ -13,41 +13,45 @@ var lmjsonParse = require("lmjson");
 var fileContents = {};
 
 function onPreprocess(text, fileName) {
-    fileContents[fileName] = text;
-    return [text];
+  fileContents[fileName] = text;
+  return [text];
 }
 
 function onPostprocess(messages, fileName) {
-    // Get document, or throw exception on error
-    try {
-        lmjsonParse(fileContents[fileName]);
-        delete fileContents[fileName];
-        return [];
-    } catch (e) {
-        delete fileContents[fileName];
-        return [{
-            ruleId: "invalid-lmjson-" + e.uwxDetail.message
-                .toLowerCase()
-                .replace(/\ba\b/g, '')
-                .replace(/[^a-z ]/g, '')
-                .trim()
-                .replace(/\s+/g, '-'),
-            severity: 2,
-            message: e.uwxDetail.message,
-            source: e.uwxDetail.text.split('\n')[e.uwxDetail.line - 1],
-            line: e.uwxDetail.line,
-            column: e.uwxDetail.col,
-        }];
-    }
+  // Get document, or throw exception on error
+  try {
+    lmjsonParse(fileContents[fileName]);
+    delete fileContents[fileName];
+    return [];
+  } catch (e) {
+    delete fileContents[fileName];
+    return [{
+      ruleId: 'invalid-lmjson-' + e.uwxDetail.message
+          .toLowerCase()
+          .replace(/\ba\b/g, '')
+          .replace(/[^a-z ]/g, '')
+          .trim()
+          .replace(/\s+/g, '-'),
+      severity: 2,
+      message: e.uwxDetail.message,
+      source: e.uwxDetail.text.split('\n')[e.uwxDetail.line - 1],
+      line: e.uwxDetail.line,
+      column: e.uwxDetail.col,
+    }];
+  }
 }
 
 var prepro = {
-    preprocess: onPreprocess,
-    postprocess: onPostprocess
+  preprocess: onPreprocess,
+  postprocess: onPostprocess
 };
 
 // import processors
 module.exports.processors = {
-    // add your processors here
-    ".lmjson": prepro,
+  // add your processors here
+  '.lmjson': prepro,
+  // sublime files are regular JSON with trailing comma support and comments
+  '.sublime-settings': prepro,
+  '.sublime-keymap': prepro,
+  '.sublime-theme': prepro,
 };
